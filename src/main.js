06539ddbf025d25e09cd6012f44e0230b27417d5
DollarBill1 <pedro1321.lucas@gmail.com>
2024-04-07 21:50:09 -0300

    Adicionando editar produto

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@
     (function start() {
         createTable();
         document.querySelector('[data-js="formCadProduct"]').addEventListener('submit', cadProduct);
+        document.querySelector('[data-js="formEditProduct"]').addEventListener('submit', EditProduct);
 
     })();
+
     function createLine(obj) {
         const tr = document.createElement('tr');
         const editAndDel = 
@@ -30,8 +32,7 @@
         var btnEdit = lastCell.firstElementChild;
         var btnDel = lastCell.lastElementChild;
 
-        btnEdit.addEventListener('click', sendDataForformEdit);
-        btnDel.addEventListener('click', sendDataForformDel);
+
         return tr;
     }
 
@@ -47,7 +48,6 @@
         thead.appendChild(tr);
         return thead;
     }
-
     function createTable() {
 
         var table = document.createElement('table');
@@ -58,6 +58,7 @@
         // requisitando do servidor
         axios.get(urlBase)
         .then((response) => {
+            console.log(response.data);
             response.data.forEach(objProd => {
                 tbody.appendChild(createLine(new Produto(objProd)));
             });
@@ -70,7 +71,10 @@
             console.error(error);
         });
     }
-
+    function injectTable(docFrag) {
+        const divToInject = document.querySelector('[data-js="injectTable"]');
+        divToInject.replaceChildren(docFrag);
+    }
     function cadProduct(evt) {
         evt.preventDefault();
         var formCadProduct = document.querySelector('[data-js="formCadProduct"]');
@@ -101,4 +105,66 @@
             console.error(error);
         }); 
     }
+    /**
+     * Function construtora para evitar bug na hora do editar 
+     * Após o envio do update na hora de requisar esses dados trás eles desordenado. 
+     */
+    function Produto(obj) {
+        obj = obj || {};
+        this.id = obj.id;
+        this.nome = obj.nome;
+        this.valor = obj.valor;
+        this.quantidadeEstoque = obj.quantidadeEstoque;
+        this.observacao = obj.observacao;
+        this.dataCadastro = obj.dataCadastro;
+    }
+    function EditProduct(evt) {
+        evt.preventDefault();
+        var formEditProduct = document.querySelector('[data-js="formEditProduct"]');
+        var inputHidden = formEditProduct.querySelector('[data-js="idPut"]');
+        var name = formEditProduct.querySelector('[data-js="name"]');
+        var price = formEditProduct.querySelector('[data-js="price"]');
+        var qtsStock = formEditProduct.querySelector('[data-js="qtsStock"]');
+        var obs = formEditProduct.querySelector('[data-js="obs"]');
+        var url = urlBase + '/' + inputHidden.value;
+
+        var productEdited = {
+            nome: name.value,
+            quantidadeEstoque: qtsStock.value,
+            valor: price.value,
+            observacao: obs.value,
+            dataCadastro: inputHidden.dataset.timestamp
+        };
+        axios.put(url, productEdited)
+        .then((response) => {
+            createTable();
+            name.value = '';
+            price.value = '';
+            qtsStock.value = '';
+            obs.value = '';
+            document.querySelector('[data-js="closeWinEdit"]').click();
+
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+    }
+    function sendDataForformEdit() {
+        var formEditProduct = document.querySelector('[data-js="formEditProduct"]');
+        var trActual = (this.parentElement).parentElement;
+        var inputHidden = formEditProduct.querySelector('[data-js="idPut"]');
+        inputHidden.value = this.dataset.id;
+
+        var name = formEditProduct.querySelector('[data-js="name"]');
+        var price = formEditProduct.querySelector('[data-js="price"]');
+        var qtsStock = formEditProduct.querySelector('[data-js="qtsStock"]');
+        var obs = formEditProduct.querySelector('[data-js="obs"]');
+        
+        name.value = trActual.cells[1].innerText;
+        price.value = trActual.cells[2].innerText;
+        qtsStock.value = trActual.cells[3].innerText;
+        obs.value = trActual.cells[4].innerText;
+        inputHidden.dataset.timestamp = trActual.cells[5].dataset.timestamp;
+    }
+
 })();
